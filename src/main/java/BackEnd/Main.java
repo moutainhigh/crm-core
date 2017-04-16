3dbb38c5cf9312766464ee3384528bd6d4d423a1
eleoleeye <insaider@tut.by>
2017-04-16 18:14:57 +0300

    base

---
+++ src/main/java/BackEnd/Main.java
@@ -0,0 +1,19 @@
+package BackEnd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by User on 16.04.2017.
+ */
+@SpringBootApplication
+public class Main {
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class, args);
+    }
+
+
+
+
+
+}
