dac104709fb4eda3fc1978f8ae36d59332fed47e
eleoleeye <insaider@tut.by>
2017-04-17 14:04:30 +0300

    dev

--- src/main/java/BackEnd/Main.java
+++ src/main/java/BackEnd/Main.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 
 /**
@@ -12,7 +12,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  */
 @SpringBootApplication
 @EnableAutoConfiguration
-public class Main extends WebMvcConfigurerAdapter  {
+public class Main extends WebMvcConfigurerAdapter {
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
     }
