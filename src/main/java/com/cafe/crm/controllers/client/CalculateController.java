3ff310e26bddb874fc1a99de5adfcdd6446c0bcc
Mariyz <mariyzdede@gmail.com>
2017-05-06 22:29:34 +0300

    вывел логику контроллеров в сервисы

--- src/main/java/com/cafe/crm/controllers/client/CalculateController.java
+++ src/main/java/com/cafe/crm/controllers/client/CalculateController.java
@@ -37,10 +37,9 @@ public class CalculateController {
 
 	@RequestMapping(value = {"/add-calculate"}, method = RequestMethod.POST)
 	//@ResponseStatus(HttpStatus.NO_CONTENT)
-	public ModelAndView addCalculate(HttpServletRequest request,
-							 @RequestParam(name = "boardId") Long id,
-							 @RequestParam(name = "number") Long number,
-							 @RequestParam(name = "description") String descr) {//передаются по полю от 3 разных сущностей
+	public ModelAndView addCalculate(@RequestParam(name = "boardId") Long id,
+									 @RequestParam(name = "number") Long number,
+									 @RequestParam(name = "description") String descr) {//передаются по полю от 3 разных сущностей
 
 		calculateControllerService.addCalculate(id, number, descr);
 		return new ModelAndView("redirect:/manager");
@@ -48,8 +47,7 @@ public class CalculateController {
 
 	@RequestMapping(value = {"/refresh-board"}, method = RequestMethod.POST)
 	@ResponseStatus(HttpStatus.NO_CONTENT)
-	public void refreshBoard(HttpServletRequest request,
-							 @RequestParam(name = "boardId") Long idB,
+	public void refreshBoard(@RequestParam(name = "boardId") Long idB,
 							 @RequestParam(name = "calculateId") Long idC) {
 
 		calculateControllerService.refreshBoard(idC, idB);
@@ -57,8 +55,7 @@ public class CalculateController {
 	}
 
 	@RequestMapping(value = {"/add-client"}, method = RequestMethod.POST)
-	public ModelAndView addClient(HttpServletRequest request,
-								  @RequestParam(name = "calculateId") Long id,
+	public ModelAndView addClient(@RequestParam(name = "calculateId") Long id,
 								  @RequestParam(name = "number") Long number,
 								  @RequestParam(name = "description") String descr) {
 
@@ -67,8 +64,7 @@ public class CalculateController {
 	}
 
 	@RequestMapping(value = {"/calculate-price"}, method = RequestMethod.POST)
-	public ModelAndView calculatePrice(HttpServletRequest request,
-									   @RequestParam(name = "discountInput") Long discount, //скидка которая написана в инпуте(не карты, а карта+скидка управляющего)
+	public ModelAndView calculatePrice(@RequestParam(name = "discountInput") Long discount, //скидка которая написана в инпуте(не карты, а карта+скидка управляющего)
 									   @RequestParam(name = "clientId") Long clientId,        // id клиента у которого считаем
 									   @RequestParam(name = "numberToCalculate") String numberToCalculate) { //количество человек для расчета клиента
 		//Long calculateId = Long.parseLong(request.getParameter("calculateId"));
--- src/main/java/com/cafe/crm/controllers/client/CardController.java
+++ src/main/java/com/cafe/crm/controllers/client/CardController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
@@ -25,33 +26,29 @@ public class CardController {
 	private CardControllerService cardControllerService;
 
 	@RequestMapping(value = {"/card"}, method = RequestMethod.GET)
-	public ModelAndView getCard(HttpServletRequest request) {
-		ModelAndView modelAndView = new ModelAndView("card");
+	public ModelAndView getCard(@RequestParam(name = "token") Long id) {
 
-		Card card = cardService.getOne(Long.parseLong(request.getParameter("token")));
+		ModelAndView modelAndView = new ModelAndView("card");
+		Card card = cardService.getOne(id);
 		modelAndView.addObject("card", card);
 		modelAndView.addObject("listCalculate", calculateService.getAll());
-
 		return modelAndView;
 	}
 
 	@RequestMapping(value = {"/add-card-to-calculate"}, method = RequestMethod.POST)
-	public ModelAndView addCardToCalculate(HttpServletRequest request) {
-		Long idCard = Long.parseLong(request.getParameter("idCard"));
-		Long idCalculate = Long.parseLong(request.getParameter("idCalculate"));
+	public ModelAndView addCardToCalculate(@RequestParam(name = "idCard") Long idCard,
+										   @RequestParam(name = "idCalculate") Long idCalculate) {
 
 		cardControllerService.addCardToCalculate(idCard, idCalculate);
-
 		return new ModelAndView("redirect:/manager");
 	}
 
 	@RequestMapping(value = {"/add-money"}, method = RequestMethod.POST)
-	public ModelAndView addMoney(HttpServletRequest request) {
-		Long idCard = Long.parseLong(request.getParameter("idCard"));
-		Double money = Double.parseDouble(request.getParameter("money"));
+	public ModelAndView addMoney(@RequestParam(name = "idCard") Long idCard,
+								 @RequestParam(name = "money") Double money) {
+
 		cardControllerService.addMoney(idCard, money);
 		return new ModelAndView("redirect:/manager");
 	}
-
 
 }
