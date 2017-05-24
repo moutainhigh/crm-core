8c9b1fb63893339192cb1109c674d6d2f5cc272f
zaytsev-dv <zaytsev.dmitry9228@gmail.com>
2017-05-24 23:33:22 +0700

    update

--- src/main/java/com/cafe/crm/security/configs/SecurityInitializer.java
+++
@@ -1,7 +0,0 @@
-package com.cafe.crm.security.configs;
-
-import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
-
-//i
-public class SecurityInitializer extends AbstractSecurityWebApplicationInitializer {
-}
