298208354225d8b2b462871ee02f7543b9c765b4
Nikita Titov <nikitaunmortal@gmail.com>
2017-06-29 14:05:06 +0300

    Standallone settings, new logger with the ability to change the logging level. prev commit was incorrect

--- src/main/java/com/cafe/crm/controllers/boss/EditController.java
+++ src/main/java/com/cafe/crm/controllers/boss/EditController.java
@@ -1,5 +1,7 @@
 package com.cafe.crm.controllers.boss;
 
+import ch.qos.logback.classic.Level;
+import ch.qos.logback.classic.Logger;
 import com.cafe.crm.models.client.Board;
 import com.cafe.crm.models.client.Calculate;
 import com.cafe.crm.models.property.PropertyWrapper;
@@ -7,11 +9,11 @@ import com.cafe.crm.service_abstract.boardService.BoardService;
 import com.cafe.crm.service_abstract.calculateService.CalculateService;
 import com.cafe.crm.service_abstract.property.PropertyService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
@@ -30,11 +32,31 @@ public class EditController {
 	@Autowired
 	private CalculateService calculateService;
 
+	@Autowired
+	@Qualifier(value = "logger")
+	private Logger log;
+
+	@Autowired
+	@Qualifier(value = "hibLogger")
+	private Logger hibLog;
+
+	@ModelAttribute(name = "logLevel")
+	public String logLevel() {
+		return log.getLevel().levelStr;
+	}
+
 	@ModelAttribute(value = "wrapper")
 	public PropertyWrapper addClass() {
 		PropertyWrapper PropertyWrapper = new PropertyWrapper();
 		PropertyWrapper.setProperties(propertyService.findAll());
 		return PropertyWrapper;
+	}
+
+	@RequestMapping(method = RequestMethod.GET)
+	public ModelAndView mainPage() {
+		ModelAndView modelAndView = new ModelAndView("bossSettingsMenu");
+		modelAndView.addObject("boards", boardService.getAll());
+		return modelAndView;
 	}
 
 	@RequestMapping(value = "/property/edit", method = RequestMethod.POST)
@@ -79,5 +101,27 @@ public class EditController {
 		boardService.save(board);
 		String referer = request.getHeader("Referer");
 		return "redirect:" + referer;
+	}
+
+	@ResponseBody
+	@RequestMapping(value = "/property/logLevel", method = RequestMethod.POST)
+	public ResponseEntity<?> setLogLevel(@RequestParam(name = "level") String logLevel) {
+		log.info("Log level: " + logLevel);
+		if (logLevel.equalsIgnoreCase("ERROR")) {
+			log.setLevel(Level.ERROR);
+			hibLog.setLevel(Level.ERROR);
+		} else if(logLevel.equalsIgnoreCase("INFO")) {
+			log.setLevel(Level.INFO);
+			hibLog.setLevel(Level.INFO);
+		} else if(logLevel.equalsIgnoreCase("DEBUG")) {
+			log.setLevel(Level.DEBUG);
+			hibLog.setLevel(Level.DEBUG);
+		} else if(logLevel.equalsIgnoreCase("TRACE")) {
+			log.setLevel(Level.TRACE);
+			hibLog.setLevel(Level.TRACE);
+		} else {
+			log.error("Unknown log level: " + logLevel);
+		}
+		return new ResponseEntity<>(HttpStatus.OK);
 	}
 }
