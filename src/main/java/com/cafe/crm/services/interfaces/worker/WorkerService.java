e70467cdf54605610de565177a8fa005d69dcaa0
Nikita Titov <nikitaunmortal@gmail.com>
2017-07-18 14:02:09 +0300

    fix creation of worker with boss and admin roles

--- src/main/java/com/cafe/crm/services/interfaces/worker/WorkerService.java
+++ src/main/java/com/cafe/crm/services/interfaces/worker/WorkerService.java
@@ -4,44 +4,47 @@ package com.cafe.crm.services.interfaces.worker;
 import com.cafe.crm.models.worker.Boss;
 import com.cafe.crm.models.worker.Manager;
 import com.cafe.crm.models.worker.Worker;
+
 import java.util.List;
 
 public interface WorkerService {
 
-	List<Worker> listAllWorker();
+    List<Worker> listAllWorker();
 
-	List<Manager> listAllManager();
+    List<Manager> listAllManager();
 
-	List<Boss> listAllBoss();
+    List<Boss> listAllBoss();
 
-	void addWorker(Worker worker);
+    void addWorker(Worker worker);
 
-	void addManager(Manager manager);
+    void addManager(Manager manager);
 
-	void addBoss(Boss boss);
+    void addBoss(Boss boss);
 
-	void editWorker(Worker worker, Long adminId, Long bossId, String password, String submitPassword);
+    void editWorker(Worker worker, Long adminId, Long bossId, String password, String submitPassword);
 
-	void editManager(Manager manager, Long adminId, Long bossId);
+    void editManager(Manager manager, Long adminId, Long bossId);
 
-	void editBoss(Boss boss, Long bossId, Long adminId, Long shiftSalary);
+    void editBoss(Boss boss, Long bossId, Long adminId, Long shiftSalary);
 
-	Worker findWorkerById(Long id);
+    Worker findWorkerById(Long id);
 
-	void deleteWorker(Worker worker);
+    void deleteWorker(Worker worker);
 
-	void castWorkerToManager(Worker worker, String password, Long adminPositionId);
+    void castWorkerToManager(Worker worker, String password, Long adminPositionId);
 
-	void castWorkerToBoss(Worker worker, String password, Long bossPositionId);
+    void castWorkerToBoss(Worker worker, String password, Long bossPositionId);
 
-	void castManagerToBoss(Manager manager, Long bossPositionId);
+    void castWorkerToBossAndAdmin(Worker worker, String password, Long bossPositionId, Long adminPositionId);
 
-	void castManagerToWorker(Manager manager);
+    void castManagerToBoss(Manager manager, Long bossPositionId);
 
-	void castBossToWorker(Boss boss);
+    void castManagerToWorker(Manager manager);
 
-	void castBossToManager(Boss boss, Long adminPositionId , Long shiftSalary);
+    void castBossToWorker(Boss boss);
 
-	String parsePhoneNumber(String phoneNumber);
+    void castBossToManager(Boss boss, Long adminPositionId, Long shiftSalary);
+
+    String parsePhoneNumber(String phoneNumber);
 
 }
