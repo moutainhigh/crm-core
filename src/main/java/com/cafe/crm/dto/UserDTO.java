39c2d174d011a6c22350450df6eba48ab005ace6
Yan <yanchumak@gmail.com>
2017-09-07 15:18:15 +0300

    add equals and hashcode to UserDTO

--- src/main/java/com/cafe/crm/dto/UserDTO.java
+++ src/main/java/com/cafe/crm/dto/UserDTO.java
@@ -1,9 +1,6 @@
 package com.cafe.crm.dto;
 
 
-import com.cafe.crm.models.user.Position;
-import com.cafe.crm.models.user.Role;
-
 import java.util.List;
 
 public class UserDTO {
@@ -133,5 +130,28 @@ public class UserDTO {
                 ", activated=" + activated +
                 ", enabled=" + enabled +
                 '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        UserDTO user = (UserDTO) o;
+
+        if (firstName != null ? !firstName.equals(user.firstName) : user.firstName != null) return false;
+        if (lastName != null ? !lastName.equals(user.lastName) : user.lastName != null) return false;
+        if (email != null ? !email.equals(user.email) : user.email != null) return false;
+        return phone != null ? phone.equals(user.phone) : user.phone == null;
+
+    }
+
+    @Override
+    public int hashCode() {
+        int result = firstName != null ? firstName.hashCode() : 0;
+        result = 31 * result + (lastName != null ? lastName.hashCode() : 0);
+        result = 31 * result + (email != null ? email.hashCode() : 0);
+        result = 31 * result + (phone != null ? phone.hashCode() : 0);
+        return result;
     }
 }
