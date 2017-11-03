e45ecacd53d84b8099ad51e9495fa68601e962b9
IvanCherepica <pivan1996@yandex.ru>
2017-11-03 21:28:21 +0300

    Date formatting was simplified

--- src/main/resources/static/js/shiftDetail.js
+++ src/main/resources/static/js/shiftDetail.js
@@ -1,17 +1,6 @@
 
 var totalPrice = 0;
 
-function formatTime(element) {
-    var elem = document.getElementById(element);
-    var d = new Date();
-    d.setTime(Date.parse(elem.textContent));
-    var dd = d.getDate();
-    var MM = d.getMonth() + 1;
-    var HH = d.getHours();
-    var mm = d.getMinutes();
-    elem.textContent = dd + "." + MM + " " + HH + ":" + mm;
-}
-
 function insertTotalPrice(clients, element) {
     document.getElementById(element).innerHTML += totalPrice;
     totalPrice = 0;
